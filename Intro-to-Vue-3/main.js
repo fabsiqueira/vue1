2c5713bee733c04c834ac4e1829f6538b835dbb8
BrenoCabral <brenoeng.cabral@gmail.com>
2021-10-03 23:07:30 -0300

    Starting the code for third example

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -3,7 +3,12 @@ const app = Vue.createApp({
         return {
             product: 'Dark Chocolate',
             image: './assets/images/dark_choc.jpg',
-            inventory: 100
+            inventory: 100,
+            details: ['60% cocoa', '30% sugar', '10% chocolate butter'],
+            variants: [
+                { id: 2234, type:'dark' },
+                { id: 2235, type:'white' }
+            ]
         }
     }
 })
