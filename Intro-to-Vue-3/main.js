96e2f5389e4fc3a95500044a3d7f25d7ca762963
fabsiqueira <fabssiqueira@hotmail.com>
2021-10-04 19:52:07 -0300

    ex2 - completo

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data(){
         return {
-            product: 'Dark Chocolate'
+            product: 'Dark Chocolate',
+            image: './assets/images/dark_choc.jpg',
+            url: 'https://www.google.com.br',
+            stock: 100
         }
     }
 })
