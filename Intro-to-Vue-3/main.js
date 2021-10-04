a5b52795c4f0c784a1859e2419487cfd647c1c7a
fabsiqueira <fabssiqueira@hotmail.com>
2021-10-04 20:10:40 -0300

    ex2 - lista

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Dark Chocolate',
             image: './assets/images/dark_choc.jpg',
             url: 'https://www.google.com.br',
-            stock: 100
+            stock: 100,
+            details: ['60% cocoa', '30% sugar' , '10% chocolate butter'],
+            variants: [
+                {id: '2234', type: "Dark"},
+                {id: '2235', type: "White"}
+            ]
         }
     }
 })
