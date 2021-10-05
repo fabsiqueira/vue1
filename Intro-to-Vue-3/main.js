28d639955418f043364fd313540bdda6062f3e5a
fabsiqueira <fabssiqueira@hotmail.com>
2021-10-04 22:06:53 -0300

    componentes

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,36 +1,14 @@
 const app = Vue.createApp({
     data(){
         return {
-            cart:0,
-            product: 'Dark Chocolate',
-            // image: './assets/images/dark_choc.jpg',
-            // inStock: true,
-            details: ['60% cocoa', '30% sugar', '10% chocolate butter'],
-            variants: [
-                { id: 2234, type:'dark', image: './assets/images/dark_choc.jpg', quantity: 50},
-                { id: 2235, type:'white', image: './assets/images/white_choc.jpg', quantity: 0 }
-            ],
-            brand: "Our Brand",
-            selectedVariant: 0
+            cart:[],
+            premium:false
         }
     },
     methods:{
-        addToCart(){
-            this.cart += 1
-        },
-        updateVariant(index){
-            this.selectedVariant = index
-        }
-    },
-    computed:{
-        title(){
-            return this.brand + " " + this.product
-        }, 
-        image(){
-            return this.variants[this.selectedVariant].image
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].quantity
+        updateCart(id){
+            this.cart.push(id)
         }
     }
+    
 })
