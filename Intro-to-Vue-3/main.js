48175cfa7b5a14c80c338ed2be1f54e1c67d6ea7
fabsiqueira <fabssiqueira@hotmail.com>
2021-10-04 21:11:34 -0300

    ex3 - completo

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -3,13 +3,34 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Dark Chocolate',
-            image: './assets/images/dark_choc.jpg',
-            inStock: true,
+            // image: './assets/images/dark_choc.jpg',
+            // inStock: true,
             details: ['60% cocoa', '30% sugar', '10% chocolate butter'],
             variants: [
-                { id: 2234, type:'dark', image: './assets/images/dark_choc.jpg' },
-                { id: 2235, type:'white', image: './assets/images/white_choc.jpg' }
-            ]
+                { id: 2234, type:'dark', image: './assets/images/dark_choc.jpg', quantity: 50},
+                { id: 2235, type:'white', image: './assets/images/white_choc.jpg', quantity: 0 }
+            ],
+            brand: "Our Brand",
+            selectedVariant: 0
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+        updateVariant(index){
+            this.selectedVariant = index
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + " " + this.product
+        }, 
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
